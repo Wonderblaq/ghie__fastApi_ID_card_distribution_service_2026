60c5842bf0125b5d3395ebf1aba8289ce1d624ee
Wonderblaq <wondersarfo0@gmail.com>
2026-04-15 15:30:52 -1200

    Added new post method for sending batch cards

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 from fastapi import Request
-
 from card_generator import generate_card
 from card_image_to_pdf import create_pdf_from_image
 from email_service import send_email_with_id
@@ -29,8 +28,21 @@ def root(name: str = "Wonder"):
     return {f"Hello {name} , welcome to your homepage"}
 
 
+# Receives Single Object from java backend, process and then send card to email
 @app.post("/create_and_send_card")
-def send_card(members: List[MemberPayload]): # Added List to handle batch request
+def send_card(members: MemberPayload):
+    # Process member data on ID card and PDF
+    image_buffer, member_data = generate_card(members.dict())
+    pdf_buffer = create_pdf_from_image(image_buffer, members.memberId)
+
+    # Send processed data to member's email and display info
+    sent = send_email_with_id(members.email, member_data, pdf_buffer)
+    return {"success" if sent else "failure": sent}
+
+
+# This endpoint allows sending cards in batches
+@app.post("/send_batch_cards")
+def send_batch_card(members: List[MemberPayload]): # Added List to handle batch request
     results = []
     for member in members:
         # Process member data on ID card and PDF
@@ -42,6 +54,7 @@ def send_card(members: List[MemberPayload]): # Added List to handle batch reques
         results.append({"email": member.email,
                         "status": "success" if sent else "failed"})
     return dict(processed_card=len(results), details=results)
+
 
 
 
